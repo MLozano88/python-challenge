89a13f3c5981bdd2573fae260b7234c605e4a6c8
Mario Lozano <m.lozano1733@gmail.com>
2020-11-29 19:35:24 -0500

    Updated the script to support lists and values in the for loop.

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,6 +3,15 @@ import csv
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
+month_change = []
+timeline = []
+
+net_total = 0
+prior_profit_loss = 0
+current_profit_loss = 0
+change = 0
+
+
 with open(csvpath) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -10,14 +19,20 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
 
     print('Financial Analysis')
-    print('---------------------------')
+    print('------------------------------------')
     months= len(list(csvreader))
     print('Total Months: '+ str(months))
 
-with open(csvpath, 'r') as f:
-  next(f)
-  total = 0
-  for row in csv.reader(f):
-    total += int(row[1])
+    for row in csvreader:
 
-  print('Total:{}'.format('$'+ str(total)))
+      net_total += int(row[1])
+
+
+
+
+
+
+
+
+    print('Total:{}'.format('$'+ str(net_total)))
+    print('------------------------------------')
