eae7e8c1be6e8e4feb8492835e163cffb130abd0
Mario Lozano <m.lozano1733@gmail.com>
2020-11-29 20:52:48 -0500

    Updated script with average change, greatest increase and decrease.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,38 +1,65 @@
 import os
 import csv
 
-csvpath = os.path.join('Resources', 'budget_data.csv')
-
-month_change = []
+#Defined variables
+profit_loss = []
 timeline = []
+
 
 net_total = 0
 prior_profit_loss = 0
 current_profit_loss = 0
 change = 0
+row_count = 0
 
+#Path used to collect data from resources
+csvpath = os.path.join('Resources', 'budget_data.csv')
 
-with open(csvpath) as csvfile:
+#Read module
+with open(csvpath, newline="") as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
     # Read the header row first (skip this step if there is now header)
-    csv_header = next(csvreader)
-
-    print('Financial Analysis')
-    print('------------------------------------')
-    months= len(list(csvreader))
-    print('Total Months: '+ str(months))
+    csv_header = next(csvfile)
 
     for row in csvreader:
 
-      net_total += int(row[1])
+      #Count through the number of rows
+      row_count += 1
 
+      current_profit_loss = int(row[1])
+      net_total += current_profit_loss
 
+      if (row_count == 1):
+        prior_profit_loss = current_profit_loss
+        continue
 
+      else:
+        change = current_profit_loss - prior_profit_loss
 
+        timeline.append(row[0])
 
+        profit_loss.append(change)
 
+        prior_profit_loss = current_profit_loss
+    
+      total_profit_loss = sum(profit_loss)
+      total_average = round(total_profit_loss/(row_count - 1), 2)
 
+      greatest_increase = max(profit_loss)
+      greatest_decrease = min(profit_loss)
 
-    print('Total:{}'.format('$'+ str(net_total)))
+      greatest_month_index = profit_loss.index(greatest_increase)
+      lowest_month_index = profit_loss.index(greatest_decrease)
+
+      most_profit = timeline[greatest_month_index]
+      most_loss = timeline[lowest_month_index]
+
+    print('Financial Analysis')
+    print('------------------------------------')
+    print(f'Total Months: {row_count}')
+    print(f'Total: ${net_total}')
+    print(f"Average Change:  ${total_average}")
+    print(f"Greatest Increase in Profits:  {most_profit} (${greatest_increase})")
+    print(f"Greatest Decrease in Profits:  {most_loss} (${greatest_decrease})")
     print('------------------------------------')
