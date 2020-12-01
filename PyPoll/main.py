e2394afef1644dd058fecff03f9dbe86b476cada
Mario Lozano <Mario@m.lozano1733.com>
2020-11-30 22:08:50 -0500

    Script was updated to calculate votes each candidate won and percentage of votes won.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,12 +1,8 @@
 import os
 import csv
 
-khan = []
-correy = []
-li = []
-tooley = []
-
-total_vote = 0
+candidate = []
+total_vote = []
 
 
 #Path used to collect data from resources
@@ -18,13 +14,31 @@ with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvfile)
-
-    for row in csvreader:
-        
-        total_vote += 1
-
     
+    #will store the contents of row 2 into a list named candidates
+    for column in csvreader:
+        candidate.append(column[2])
+        total_vote.append(column[0])
+
+    vote_count = len(total_vote)
+    #counts the number of times the candidate's name appears in column 2
+    khan_votes = int(candidate.count("Khan"))
+    correy_votes = int(candidate.count("Correy"))
+    li_votes = int(candidate.count("Li"))
+    tooley_votes = int(candidate.count("O'Tooley"))
+
+    #calculate the percentage of a candidate's vote total
+    k_percentage = round((khan_votes/vote_count) * 100)
+    c_percentage = round((correy_votes/vote_count) * 100)
+    l_percentage = round((li_votes/vote_count) * 100)
+    t_percentage = round((tooley_votes/vote_count) * 100)
+
     print('Election Results')
     print('------------------------------')
-    print(f'Total Votes: {total_vote}')
+    print(f'Total Votes: {vote_count}')
+    print('------------------------------')
+    print(f"Khan: {k_percentage}% ({khan_votes})")
+    print(f"Correy: {c_percentage}% ({correy_votes})")
+    print(f"Li: {l_percentage}% ({li_votes})")
+    print(f"O'Tooley: {t_percentage}% ({tooley_votes})")
     print('------------------------------')
